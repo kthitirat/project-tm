6d9774b8add02f0b9d9a77996f96abc91133121f
kthitirat <kthitirat@aru.ac.th>
2024-05-23 15:38:06 +0700

    feat: relationship

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -10,4 +10,9 @@ class Department extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function professors()
+    {
+        return $this->hasMany(Professor::class, 'department_id');
+    }
 }
--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -10,4 +10,9 @@ class Professor extends Model
     use HasFactory;
 
     protected $fillable = ['department_id', 'major', 'prefix', 'first_name', 'last_name'];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
 }
